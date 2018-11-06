b6797259b4df73ffdbdd92da7479cf4e8dc72c32
Bret Barkelew <brbarkel@microsoft.com>
2018-11-06 18:34:49 +0000

    Merged PR 391: Improve Tiano implementation of FmpDxe.
    Add a PCD control for ESRT/capsule targeting GUIDs. This enabled the FILE_GUID to remain consistent for variable access while the ESRT GUID can periodically change if it is needed to ensure capsule ordering.
    
    Skip the DetectTestKey logic which currently requires additional crypto functions (ultimately add to shared protocol).
    Add enhanced comments to easy identification of failures in FMP drivers.

--- FmpDevicePkg/FmpDxe/FmpDxe.c
+++ FmpDevicePkg/FmpDxe/FmpDxe.c
@@ -181,7 +181,10 @@ GetImageTypeIdGuid (
     if (ImageTypeIdGuidSize == sizeof (EFI_GUID)) {
       FmpDeviceLibGuid = (EFI_GUID *)PcdGetPtr (PcdFmpDeviceImageTypeIdGuid);
     } else {
-      DEBUG ((DEBUG_WARN, "FmpDxe(%s): Fall back to ImageTypeIdGuid of gEfiCallerIdGuid\n", mImageIdName));
+      // MU_CHANGE start - this is a misconfiguration error, we should assert
+      ASSERT (FALSE);
+      DEBUG ((DEBUG_ERROR, "FmpDxe(%s): Fall back to ImageTypeIdGuid of gEfiCallerIdGuid. FmpDxe error: misconfiguration\n", mImageIdName));
+      // MU_CHANGE end
       FmpDeviceLibGuid = &gEfiCallerIdGuid;
     }
   }
