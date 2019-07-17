612e17512075835317bd4d50e40e60a1d1bc09fd
Bret Barkelew <brbarkel@microsoft.com>
2019-07-16 17:51:03 -0700

    REBASE: Added documentation for 2002 release

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -161,12 +161,12 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "Silicon/Arm/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_silicon_arm_tiano.git",
-                "Branch": ""
+                "Branch": "rebase/202002/ci2"
             },
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": ""
+                "Branch": "release/202002"
             }
         ]
         # MU_CHANGE END
