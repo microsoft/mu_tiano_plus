232e35cd0587a9c2997fa3b0b3358bf2f9dde4a5
Bret Barkelew <brbarkel@microsoft.com>
2020-03-06 18:02:37 -0800

    SecurityPkg: Add Tpm2DebugLib to support detailed logging
    Having various modes for TPM logging aids in a lot debugging
    scenarios.
    
    Signed-off-by: Bret Barkelew <brbarkel@microsoft.com>
    
    cherry-pick from 2c67a45495

---
+++ SecurityPkg/Include/Library/Tpm2DebugLib.h
@@ -0,0 +1,43 @@
+/** @file -- Tpm2DebugLib.h
+This file contains helper functions to perform a detailed debugging of
+TPM transactions as they go to and from the TPM device.
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef TPM_2_DEBUG_LIB_H_
+#define TPM_2_DEBUG_LIB_H_
+
+/**
+  This function dumps as much information as possible about
+  a command being sent to the TPM for maximum user-readability.
+
+  @param[in]  InputBlockSize  Size of the input buffer.
+  @param[in]  InputBlock      Pointer to the input buffer itself.
+
+**/
+VOID
+EFIAPI
+DumpTpmInputBlock (
+  IN UINT32       InputBlockSize,
+  IN CONST UINT8  *InputBlock
+  );
+
+/**
+  This function dumps as much information as possible about
+  a response from the TPM for maximum user-readability.
+
+  @param[in]  OutputBlockSize  Size of the output buffer.
+  @param[in]  OutputBlock      Pointer to the output buffer itself.
+
+**/
+VOID
+EFIAPI
+DumpTpmOutputBlock (
+  IN UINT32       OutputBlockSize,
+  IN CONST UINT8  *OutputBlock
+  );
+
+#endif // TPM_2_DEBUG_LIB_H_
