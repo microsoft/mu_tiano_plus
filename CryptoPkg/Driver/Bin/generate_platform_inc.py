8d054b4a41f04b70f30dde59713e6f62a509eb3f
Matthew Carlson <macarl@microsoft.com>
2020-03-27 19:10:27 -0700

    Added debug?

--- CryptoPkg/Driver/Bin/generate_platform_inc.py
+++ CryptoPkg/Driver/Bin/generate_platform_inc.py
@@ -57,7 +57,7 @@ def main():
                           f"ENTRY_POINT                    = Crypto{phase}Entry"])
         inf_lines.append("\n[Binaries]")
         inf_lines.append(
-            f"  DXE_DEPEX|edk2-basecrypto-driver-bin_extdep/{flavor}/{target}/Crypto{phase}.depex")
+            f"  DXE_DEPEX|edk2-basecrypto-driver-bin_extdep/{flavor}/{target}/Crypto{phase}.depex|{target}")
         for arch in arches:
             inf_lines.append(f"\n[Binaries.{arch}]")
             inf_lines.append(
