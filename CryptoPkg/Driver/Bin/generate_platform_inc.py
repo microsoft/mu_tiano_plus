b8ccf97fa2c4c05d0e4feb6bb02e178a4253aca9
Matthew Carlson <macarl@microsoft.com>
2020-03-30 22:19:13 -0700

    Took out more defines and added an error in its place

--- CryptoPkg/Driver/Bin/generate_platform_inc.py
+++ CryptoPkg/Driver/Bin/generate_platform_inc.py
@@ -124,7 +124,7 @@ def main():
     for phase in phases:
         phase = phase.upper()
         dsc_lines.append(f"!ifndef {phase}_CRYPTO_SERVICES")
-        dsc_lines.append(f" !DEFINE {phase}_CRYPTO_SERVICES = ALL")
+        dsc_lines.append(f" !error Please define {phase}_CRYPTO_SERVICES")
         dsc_lines.append("!endif")
         dsc_lines.append(f"!if $({phase}_CRYPTO_SERVICES) IN \"{all_flavors}\"")
         dsc_lines.append(" # we don't have a problem")
