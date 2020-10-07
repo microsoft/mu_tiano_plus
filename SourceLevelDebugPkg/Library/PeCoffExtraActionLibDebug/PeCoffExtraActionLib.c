d5b5ad7b5c0acda1eecbdd28b46a3e3d6281c79c
Matthew Carlson <macarl@microsoft.com>
2020-10-06 19:52:22 -0700

    (SourceLevelDebugPkg) Fix issue in PeCoffExtraActionLib around IO_HW_BREAKPOINT_VECTOR_NUM not hooking IDT entry for INT1

--- SourceLevelDebugPkg/Library/PeCoffExtraActionLibDebug/PeCoffExtraActionLib.c
+++ SourceLevelDebugPkg/Library/PeCoffExtraActionLibDebug/PeCoffExtraActionLib.c
@@ -64,7 +64,7 @@ PeCoffLoaderExtraActionCommon (
   ASSERT (ImageContext != NULL);
 
   if (ImageContext->PdbPointer != NULL) {
-    DEBUG ((DEBUG_ERROR, "    PDB = %a\n", ImageContext->PdbPointer));
+    DEBUG ((EFI_D_INFO, "    PDB = %a\n", ImageContext->PdbPointer));     // MS_CHANGE_304324
   }
 
   //
@@ -95,11 +95,16 @@ PeCoffLoaderExtraActionCommon (
     }
   } else {
     if (!CheckDebugAgentHandler (&IdtDescriptor, IO_HW_BREAKPOINT_VECTOR_NUM)) {
+      // MU_CHANGE START 217204
+      goto Cleanup;
+ #if 0
       //
       // Save and update IDT entry for INT1
       //
       SaveAndUpdateIdtEntry1 (&IdtDescriptor, &OriginalIdtEntry);
       IdtEntryHooked = TRUE;
+ #endif
+      // MU_CHANGE END
     }
   }
 
@@ -183,6 +188,8 @@ PeCoffLoaderExtraActionCommon (
       AsmWriteDr7 (Dr7);
     }
   }
+
+Cleanup:                               // MS_CHANGE_217204
 
   //
   // Restore original IDT entry for INT1 if it was hooked.
