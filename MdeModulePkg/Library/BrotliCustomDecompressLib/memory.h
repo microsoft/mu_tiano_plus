f539ec25beacd05ea88edcb3166222d0d7a54de6
brbarkel@microsoft.com <brbarkel@microsoft.com>
2021-09-03 12:01:36 -0700

    TCMORPH: Remove unused packages from Tiano (command in comments).
    1) Update .gitignore with an accurate list of files we don't want.
    2) git ls-files -c -i --exclude-from=.gitignore | xargs git rm --cached
    3) Above command must be issued from Git Bash, not PowerShell et al.

--- MdeModulePkg/Library/BrotliCustomDecompressLib/memory.h
+++
@@ -1,9 +0,0 @@
-/** @file
-  Include file to support building the third-party brotli.
-
-  Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.<BR>
-  SPDX-License-Identifier: BSD-2-Clause-Patent
-
-**/
-
-#include <BrotliDecUefiSupport.h>
--- MdeModulePkg/Library/BrotliCustomDecompressLib/sys/types.h
+++
@@ -1,9 +0,0 @@
-/** @file
-  Include file to support building the third-party brotli.
-
-Copyright (c) Microsoft Corporation.
-SPDX-License-Identifier: BSD-2-Clause-Patent
-
-**/
-
-#include <brotli/types.h>
