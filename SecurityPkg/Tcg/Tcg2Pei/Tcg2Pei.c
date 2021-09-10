microsoft/mu_basecore

Merged PR 1892499: Add support for Excluded Fvs in Dxe Tpm2 MeasuredBootLib
Add support for Excluded Fvs in Dxe Tpm2 MeasuredBootLib

Related work items: #17699630

SecurityPkg/Tcg/Tcg2Pei/Tcg2Pei.c
@@ -26,6 +26,7 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 #include <Guid/TcgEventHob.h>
 #include <Guid/MeasuredFvHob.h>
+#include <Guid/ExcludedFvHob.h> //Mschange
 #include <Guid/TpmInstance.h>
 
 #include <Library/DebugLib.h>
@@ -174,10 +175,19 @@ EndofPeiSignalNotifyCallBack (
   )
 {  
   MEASURED_HOB_DATA *MeasuredHobData;
+  EXCLUDED_HOB_DATA *ExcludedHobData;  //mschange
+  EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_PPI *MeasurementExcludedFvPpi; //mschange
+  UINT32 Instance; //mschange
+  UINT32 Count; //mschange
+  UINT32 HobIndex; //mschange
+  EFI_STATUS Status; //mschange
+
 
   PERF_CALLBACK_BEGIN(PERF_VERBOSITY_STANDARD, &gEfiEndOfPeiSignalPpiGuid); // MS_CHANGE
 
   MeasuredHobData = NULL;
+  ExcludedHobData = NULL;
+  MeasurementExcludedFvPpi = NULL;
 
   //
   // Create a Guid hob to save all measured Fv 
@@ -203,6 +213,81 @@ EndofPeiSignalNotifyCallBack (
     //
     CopyMem (&MeasuredHobData->MeasuredFvBuf[mMeasuredBaseFvIndex] , mMeasuredChildFvInfo, sizeof(EFI_PLATFORM_FIRMWARE_BLOB) * (mMeasuredChildFvIndex));
   }
+
+  //
+  // Create a guid hob to save all excluded FVs for DXE - mschange start
+  //
+
+  //
+  //Get count
+  //
+  Instance = 0;
+  Count = 0;
+  do {
+    Status = PeiServicesLocatePpi(
+                 &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+                 Instance,
+                 NULL,
+                 (VOID**)&MeasurementExcludedFvPpi
+                 );
+    if (!EFI_ERROR(Status)) {
+      Count += MeasurementExcludedFvPpi->Count;
+      Instance++;
+    }
+  } while (!EFI_ERROR(Status));
+
+  // Check if there are any excluded FVs
+  if(Count > 0)
+  {
+    DEBUG((DEBUG_INFO, "Found %d FVs excluded.  Publishing hob\n", Count));
+    ExcludedHobData = BuildGuidHob(
+                      &gExcludedFvHobGuid,
+                      sizeof(EXCLUDED_HOB_DATA) + ( sizeof(EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)
+                      );
+
+    //Make sure allocation was successful
+    if(ExcludedHobData != NULL)
+    {
+      ExcludedHobData->Num = Count;
+      //
+      //Copy FV info to hob data
+      //
+      Instance = 0;
+      HobIndex = 0;
+      do {
+        Status = PeiServicesLocatePpi(
+                    &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+                    Instance,
+                    NULL,
+                    (VOID**)&MeasurementExcludedFvPpi
+                    );
+        if (!EFI_ERROR(Status)) {
+          if(MeasurementExcludedFvPpi->Count <= 0)
+          {
+            DEBUG((DEBUG_ERROR, "ExcludedFvPpi has invalid count %d", MeasurementExcludedFvPpi->Count));
+            ASSERT(MeasurementExcludedFvPpi->Count > 0);
+          } 
+          else if( HobIndex + MeasurementExcludedFvPpi->Count > Count) 
+          {
+            DEBUG((DEBUG_ERROR, "Found more ExcludedFvPpi fvs than when calculated buffer size.  BufferSizeCount (0x%x)  NewCount (0x%x)", Count, (HobIndex + MeasurementExcludedFvPpi->Count)));
+            ASSERT(HobIndex + MeasurementExcludedFvPpi->Count <= Count);
+          } 
+          else 
+          {
+            CopyMem(&ExcludedHobData->ExcludedFvs[HobIndex], &MeasurementExcludedFvPpi->Fv[0], sizeof(EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * MeasurementExcludedFvPpi->Count);
+            HobIndex += MeasurementExcludedFvPpi->Count;
+          }
+          Instance++;
+        }
+      } while (!EFI_ERROR(Status));
+    }
+    else
+    {
+      DEBUG((DEBUG_ERROR, "Failed to allocate 0x%x byte of memory for ExcludedFvHob", 
+        sizeof(EXCLUDED_HOB_DATA) + ( sizeof(EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)));
+    }
+  }  //Done with Excluded Fv Hob - mschange end
+
 
   PERF_CALLBACK_END(PERF_VERBOSITY_STANDARD, &gEfiEndOfPeiSignalPpiGuid); // MS_CHANGE
 

microsoft/mu_tiano_plus

SecurityPkg/Tcg/Tcg2Pei/Tcg2Pei.c
@@ -21,6 +21,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Guid/TcgEventHob.h>
 #include <Guid/MeasuredFvHob.h>
+#include <Guid/ExcludedFvHob.h> //Mschange
 #include <Guid/TpmInstance.h>
 #include <Guid/MigratedFvInfo.h>
 
@@ -232,11 +233,19 @@ EndofPeiSignalNotifyCallBack (
   IN VOID                          *Ppi
   )
 {
-  MEASURED_HOB_DATA *MeasuredHobData;
+  MEASURED_HOB_DATA                                     *MeasuredHobData;
+  EXCLUDED_HOB_DATA                                     *ExcludedHobData;          // MS_CHANGE
+  EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_PPI *MeasurementExcludedFvPpi; // MS_CHANGE
+  UINT32                                                Instance;                  // MS_CHANGE
+  UINT32                                                Count;                     // MS_CHANGE
+  UINT32                                                HobIndex;                  // MS_CHANGE
+  EFI_STATUS                                            Status;                    // MS_CHANGE
 
   PERF_CALLBACK_BEGIN(PERF_VERBOSITY_STANDARD, &gEfiEndOfPeiSignalPpiGuid); // MS_CHANGE
 
   MeasuredHobData = NULL;
+  ExcludedHobData = NULL;         // MS_CHANGE
+  MeasurementExcludedFvPpi = NULL;// MS_CHANGE
 
   PERF_CALLBACK_BEGIN (&gEfiEndOfPeiSignalPpiGuid);
 
@@ -265,6 +274,81 @@ EndofPeiSignalNotifyCallBack (
     CopyMem (&MeasuredHobData->MeasuredFvBuf[mMeasuredBaseFvIndex] , mMeasuredChildFvInfo, sizeof(EFI_PLATFORM_FIRMWARE_BLOB) * (mMeasuredChildFvIndex));
   }
 
+  // MS_CHANGE - START
+  // Create a guid hob to save all excluded FVs for DXE - mschange start
+  //
+
+  //
+  //Get count
+  //
+  Instance = 0;
+  Count = 0;
+  do {
+    Status = PeiServicesLocatePpi(
+                 &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+                 Instance,
+                 NULL,
+                 (VOID**)&MeasurementExcludedFvPpi
+                 );
+    if (!EFI_ERROR(Status)) {
+      Count += MeasurementExcludedFvPpi->Count;
+      Instance++;
+    }
+  } while (!EFI_ERROR(Status));
+
+  // Check if there are any excluded FVs
+  if(Count > 0)
+  {
+    DEBUG((DEBUG_INFO, "Found %d FVs excluded.  Publishing hob\n", Count));
+    ExcludedHobData = BuildGuidHob(
+                      &gExcludedFvHobGuid,
+                      sizeof(EXCLUDED_HOB_DATA) + ( sizeof(EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)
+                      );
+
+    //Make sure allocation was successful
+    if(ExcludedHobData != NULL)
+    {
+      ExcludedHobData->Num = Count;
+      //
+      //Copy FV info to hob data
+      //
+      Instance = 0;
+      HobIndex = 0;
+      do {
+        Status = PeiServicesLocatePpi(
+                    &gEfiPeiFirmwareVolumeInfoMeasurementExcludedPpiGuid,
+                    Instance,
+                    NULL,
+                    (VOID**)&MeasurementExcludedFvPpi
+                    );
+        if (!EFI_ERROR(Status)) {
+          if(MeasurementExcludedFvPpi->Count <= 0)
+          {
+            DEBUG((DEBUG_ERROR, "ExcludedFvPpi has invalid count %d", MeasurementExcludedFvPpi->Count));
+            ASSERT(MeasurementExcludedFvPpi->Count > 0);
+          } 
+          else if( HobIndex + MeasurementExcludedFvPpi->Count > Count) 
+          {
+            DEBUG((DEBUG_ERROR, "Found more ExcludedFvPpi fvs than when calculated buffer size.  BufferSizeCount (0x%x)  NewCount (0x%x)", Count, (HobIndex + MeasurementExcludedFvPpi->Count)));
+            ASSERT(HobIndex + MeasurementExcludedFvPpi->Count <= Count);
+          } 
+          else 
+          {
+            CopyMem(&ExcludedHobData->ExcludedFvs[HobIndex], &MeasurementExcludedFvPpi->Fv[0], sizeof(EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * MeasurementExcludedFvPpi->Count);
+            HobIndex += MeasurementExcludedFvPpi->Count;
+          }
+          Instance++;
+        }
+      } while (!EFI_ERROR(Status));
+    }
+    else
+    {
+      DEBUG((DEBUG_ERROR, "Failed to allocate 0x%x byte of memory for ExcludedFvHob", 
+        sizeof(EXCLUDED_HOB_DATA) + ( sizeof(EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV) * Count)));
+    }
+  }  //Done with Excluded Fv Hob - mschange end
+
+  //MS_CHANGE - END
   PERF_CALLBACK_END (&gEfiEndOfPeiSignalPpiGuid);
 
   return EFI_SUCCESS;