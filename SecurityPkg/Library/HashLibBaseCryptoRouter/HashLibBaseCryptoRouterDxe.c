2cbe64faa4c99d73a6589bc3e8b533e6877b1d21
Michael Kubacki <michael.kubacki@microsoft.com>
2022-09-15 01:43:45 +0000

    Merged PR 6023: CodeQL Fixes
    ## Description
    
    Resolves CodeQL issues.
    
    Another lighter pass of CodeQL will occur shortly in the future.
    
    - [ ] Breaking change?
      - Will this change break pre-existing builds or functionality without action being taken?
      **No**
    
    ## How This Was Tested
    
    - Verified CI build
    - Checked before and after CodeQL results
    - Verified boot on QemuQ35Pkg
    
    ## Integration Instructions
    
    N/A - If CodeQL was run before these changes, run again to get latest results
    
    Co-authored-by: Doug Flick <dougflick@microsoft.com>
    Co-authored-by: Kun Qin <kuqin@microsoft.com>
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- SecurityPkg/Library/HashLibBaseCryptoRouter/HashLibBaseCryptoRouterDxe.c
+++ SecurityPkg/Library/HashLibBaseCryptoRouter/HashLibBaseCryptoRouterDxe.c
@@ -79,6 +79,11 @@ HashStart (
 
   for (Index = 0; Index < mHashInterfaceCount; Index++) {
     HashMask = Tpm2GetHashMaskFromAlgo (&mHashInterface[Index].HashGuid);
+    if (HashCtx == NULL) {
+      // If we fail to get the hash mask we don't have resources.
+      return EFI_OUT_OF_RESOURCES;
+    }
+
     if ((HashMask & PcdGet32 (PcdTpm2HashMask)) != 0) {
       mHashInterface[Index].HashInit (&HashCtx[Index]);
     }
@@ -210,8 +215,16 @@ HashAndExtend (
 
   CheckSupportedHashMaskMismatch ();
 
-  HashStart (&HashHandle);
-  HashUpdate (HashHandle, DataToHash, DataToHashLen);
+  Status = HashStart (&HashHandle);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status = HashUpdate (HashHandle, DataToHash, DataToHashLen);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
   Status = HashCompleteAndExtend (HashHandle, PcrIndex, NULL, 0, DigestList);
 
   return Status;
