f2d2b20bfbdc08228a59855edd10f730bbbd22a1
Taylor Beebe <31827475+TaylorBeebe@users.noreply.github.com>
2022-11-08 11:56:07 -0800

    PrmPkg: Don't Set Access Attributes of Runtime MMIO Ranges (#51)
    Passing in access attributes to SetMemorySpaceAttributes() will cause
    the existing attributes to be overwritten. The MMIO region should have
    the appropriate attributes applied during memory protection
    initialization and the attributes of the memory space descriptor are
    inaccurate. Don't pass in any CPU arch attributes so SetMemorySpaceAttributes()
    doesn't subsequently call gCpu->SetMemoryAttributes().
    
    No
    
    - Booting to the OS and running the paging audit app on Q35
    
    N/A

--- PrmPkg/PrmConfigDxe/PrmConfigDxe.c
+++ PrmPkg/PrmConfigDxe/PrmConfigDxe.c
@@ -155,7 +155,14 @@ SetRuntimeMemoryRangeAttributes (
     Status = gDS->SetMemorySpaceAttributes (
                     RuntimeMmioRanges->Range[Index].PhysicalBaseAddress,
                     (UINT64)RuntimeMmioRanges->Range[Index].Length,
-                    Descriptor.Attributes | EFI_MEMORY_RUNTIME
+                    // MU_CHANGE START: The memory space descriptor access attributes are not accurate. Don't pass
+                    //                  in access attributes so SetMemorySpaceAttributes() doesn't update them.
+                    //                  EFI_MEMORY_RUNTIME is not a CPU arch attribute, so calling
+                    //                  SetMemorySpaceAttributes() with only it set will not clear existing page table
+                    //                  attributes for this region, such as EFI_MEMORY_XP
+                    // Descriptor.Attributes | EFI_MEMORY_RUNTIME
+                    EFI_MEMORY_RUNTIME
+                    // MU_CHANGE END
                     );
     ASSERT_EFI_ERROR (Status);
     if (EFI_ERROR (Status)) {
