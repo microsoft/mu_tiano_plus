8d16acb8538b6d474f0f61bc2ed56efa52931431
Ken Lautner <klautner@microsoft.com>
2023-05-10 13:59:49 -0700

    Fixed CISettings to look at the new mu_basecore branch

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -230,7 +230,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": "release/202208"
+                "Branch": "release/202302"
             }
         ]
         # MU_CHANGE END
