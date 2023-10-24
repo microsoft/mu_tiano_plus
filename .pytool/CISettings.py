8f7bc7fe679a27930bcc4970e2460f65a3398830
Ken Lautner <klautner@microsoft.com>
2023-10-23 17:04:38 -0700

    TEMPORARY COMMIT: POINT TOWARD REBASE MU_BASECORE BRANCH

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -232,7 +232,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": ""
+                "Branch": "personal/klautner/rebase/2308_Staging_2"
             }
         ]
         # MU_CHANGE END
