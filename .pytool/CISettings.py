5582de899e39e1c057b6b82662e96e988f77cb24
Ken Lautner <klautner@microsoft.com>
2023-12-19 09:23:16 -0800

    Updated CISettings.py to use the 2311_Staging branch for MU_BASECORE

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -234,7 +234,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": "personal/klautner/rebase/2308_Staging_2"
+                "Branch": "personal/klautner/rebase/2311_Staging"
             }
         ]
         # MU_CHANGE END
