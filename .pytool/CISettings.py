bbdb1b5631d231e493bf58863f5d749db38a7648
Ken Lautner <klautner@microsoft.com>
2024-01-18 21:19:44 -0800

    Change submodule to look at the new release branch

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -234,7 +234,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": "personal/klautner/rebase/2311_Staging"
+                "Branch": "release/202311"
             }
         ]
         # MU_CHANGE END
