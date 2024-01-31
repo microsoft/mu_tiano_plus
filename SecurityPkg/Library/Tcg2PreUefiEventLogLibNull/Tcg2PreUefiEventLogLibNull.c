55158310440d392f13c30bc14743dc95f3ec7352
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2024-01-31 10:32:27 -0800

    Added Tcg2PreUefiEventLogLibNull

---
+++ SecurityPkg/Library/Tcg2PreUefiEventLogLibNull/Tcg2PreUefiEventLogLibNull.c
@@ -0,0 +1,20 @@
+/** @file Tcg2PreUefiEventLogLibNull.c
+NOTE: NULL Tcg2PreUefiEventLogLibNull library instance
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/DebugLib.h>
+
+/**
+  Create the EventLog entries.
+**/
+VOID
+CreateTcg2PreUefiEventLogEntries (
+  VOID
+  )
+{
+  return;
+}
