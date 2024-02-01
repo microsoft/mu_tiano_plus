aab3bf31c55d6fa1a8851c8f863cb7dd05393b3b
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2024-01-31 16:53:15 -0800

    Resolved PR comments - Added EFIAPI, removed debuglib, suggested changes in the comment sections

--- SecurityPkg/Library/Tcg2PreUefiEventLogLibNull/Tcg2PreUefiEventLogLibNull.c
+++ SecurityPkg/Library/Tcg2PreUefiEventLogLibNull/Tcg2PreUefiEventLogLibNull.c
@@ -1,17 +1,16 @@
 /** @file Tcg2PreUefiEventLogLibNull.c
-NOTE: NULL Tcg2PreUefiEventLogLibNull library instance
+ NULL Tcg2PreUefiEventLogLibNull library instance
 
 Copyright (c) Microsoft Corporation. All rights reserved.
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
-#include <Library/DebugLib.h>
-
 /**
   Create the EventLog entries.
 **/
 VOID
+EFIAPI
 CreateTcg2PreUefiEventLogEntries (
   VOID
   )
