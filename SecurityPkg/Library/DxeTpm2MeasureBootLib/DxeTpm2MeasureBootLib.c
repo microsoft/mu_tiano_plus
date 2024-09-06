ceb2ffa3be875fede7b6e8448504eb0fe5bfaad8
Doug Flick <dougflick@microsoft.com>
2024-09-05 23:30:30 -0700

    change2 : Minimal change that adds the same behavior

--- SecurityPkg/Library/DxeTpm2MeasureBootLib/DxeTpm2MeasureBootLib.c
+++ SecurityPkg/Library/DxeTpm2MeasureBootLib/DxeTpm2MeasureBootLib.c
@@ -47,7 +47,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/SecurityManagementLib.h>
 #include <Library/HobLib.h>
 #include <Protocol/CcMeasurement.h>
-#include <Protocol/FirmwareVolume2.h>
 
 #include "DxeTpm2MeasureBootLibSanitization.h"
 
@@ -766,22 +765,8 @@ DxeTpm2MeasureBootHandler (
     //      From measured firmware volume: do not measure
     //      From unmeasured firmware volume: measure
     //
-    ApplicationRequired = TRUE;
-
-    // Check that the EFI Application comes from measured firmware
-    // TODO - This certainly checks the the application originated from FV - how do I confirm this is measured and not unmeasured
-    Status = gBS->OpenProtocol (
-                    Handle,
-                    &gEfiFirmwareVolume2ProtocolGuid,
-                    NULL,
-                    NULL,
-                    NULL,
-                    EFI_OPEN_PROTOCOL_TEST_PROTOCOL
-                    );
-    if (!EFI_ERROR (Status)) {
-      return EFI_SUCCESS;
-    }
     // MU_CHANGE [END]
+    ApplicationRequired = TRUE;
 
     if ((mTcg2CacheMeasuredHandle != Handle) && (mTcg2MeasuredHobData != NULL)) {
       //
@@ -875,8 +860,10 @@ DxeTpm2MeasureBootHandler (
   // Measure only application if Application flag is set
   // Measure drivers and applications if Application flag is not set
   //
-  if ((!ApplicationRequired) ||
-      (ApplicationRequired && (ImageContext.ImageType == EFI_IMAGE_SUBSYSTEM_EFI_APPLICATION)))
+  // MU_CHANGE
+  //if ((!ApplicationRequired) ||
+  //    (ApplicationRequired && (ImageContext.ImageType == EFI_IMAGE_SUBSYSTEM_EFI_APPLICATION)))
+  if (!ApplicationRequired)
   {
     //
     // Print the image path to be measured.
@@ -907,6 +894,28 @@ DxeTpm2MeasureBootHandler (
                DevicePathNode
                );
   }
+
+  // MU_CHANGE [END]
+  DEBUG_CODE_BEGIN ();
+  //
+  // Due to "TCG_SPEC_CHANGE: PCR4_SKIP_FV_APP" applications launched from measured firmware will be skipped
+  //
+
+  if (ApplicationRequired && (ImageContext.ImageType == EFI_IMAGE_SUBSYSTEM_EFI_APPLICATION)) {
+      CHAR16  *ToText;
+      ToText = ConvertDevicePathToText (
+                DevicePathNode,
+                FALSE,
+                TRUE
+                );
+      if (ToText != NULL) {
+        DEBUG ((DEBUG_INFO, "TCG_SPEC_CHANGE: PCR4_SKIP_FV_APP -- Skipping: %s.\n", ToText));
+        FreePool (ToText);
+    }
+  }
+
+  DEBUG_CODE_END ();
+  // MU_CHANGE [END]
 
   //
   // Done, free the allocated resource.
