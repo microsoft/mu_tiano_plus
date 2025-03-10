05d5e30cc015bbe525585ce33ba10738efb42e39
rohansen <rohansen@microsoft.com>
2025-03-10 20:22:28 +0000

    Handle EfiGcdMemoryTypeNonExistent

--- PrmPkg/PrmConfigDxe/PrmConfigDxe.c
+++ PrmPkg/PrmConfigDxe/PrmConfigDxe.c
@@ -100,18 +100,22 @@ SetRuntimeMemoryRangeAttributes (
 
     Status2 = EFI_NOT_FOUND;
     Status  = gDS->GetMemorySpaceDescriptor (RuntimeMmioRanges->Range[Index].PhysicalBaseAddress, &Descriptor);
-    if (!EFI_ERROR (Status) &&
+    if (!EFI_ERROR (Status) && (Descriptor.GcdMemoryType == EfiGcdMemoryTypeNonExistent)) 
+    {
+      // The MMIO range is visible but has not been added to the memory space treat the region as not found.
+      Status = EFI_NOT_FOUND;
+    } else if (!EFI_ERROR (Status) &&
         (
          ((Descriptor.GcdMemoryType != EfiGcdMemoryTypeMemoryMappedIo) && (Descriptor.GcdMemoryType != EfiGcdMemoryTypeReserved)) ||
          ((Descriptor.Length & EFI_PAGE_MASK) != 0)
         )
-        )
+      )
     {
       Status2 =  gDS->RemoveMemorySpace (
                         RuntimeMmioRanges->Range[Index].PhysicalBaseAddress,
                         Descriptor.Length
                         );
-    }
+    } 
 
     if ((Status == EFI_NOT_FOUND) || !EFI_ERROR (Status2)) {
       Status = gDS->AddMemorySpace (
