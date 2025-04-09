c702a8f05fce3c12809d329dd327c61d770d8341
Aaron Pop <aaronpop@microsoft.com>
2025-04-09 13:37:08 -0700

    Run CI using release branches of dependent repos

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -195,7 +195,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": "dev/202502"
+                "Branch": "release/202502"
             }
         ]
 
