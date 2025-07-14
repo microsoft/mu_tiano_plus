2ae53432f7ec81ba573c138f4e4f6a64c828b185
Oliver Smith-Denny <osde@microsoft.com>
2025-07-14 10:52:15 -0700

    Revert "SecurityPkg: Tpm2DebugLib: Move Noisy Debug Prints to Verbose Lib"
    This reverts commit 830d5b21741f3a583e745a8394733b3b2f36ddc1 as it
    has been upstreamed to edk2 in a different form.

--- SecurityPkg/Library/Tpm2CommandLib/Tpm2Integrity.c
+++ SecurityPkg/Library/Tpm2CommandLib/Tpm2Integrity.c
@@ -8,7 +8,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <IndustryStandard/UefiTcgPlatform.h>
 #include <Library/Tpm2CommandLib.h>
-#include <Library/Tpm2DebugLib.h> // MU_CHANGE: Move noisy prints to debug lib
 #include <Library/Tpm2DeviceLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/BaseLib.h>
@@ -758,8 +757,7 @@ Tpm2PcrReadForActiveBank (
   UINT32              ActivePcrBanks;
   UINT32              TcgRegistryHashAlg;
   UINTN               Index;
-
-  // UINTN               Index2; MU_CHANGE: Use debug lib
+  UINTN               Index2;
 
   PcrIndex = (UINT8)PcrHandle;
 
@@ -870,27 +868,20 @@ Tpm2PcrReadForActiveBank (
     return EFI_DEVICE_ERROR;
   }
 
-  // MU_CHANGE Start: Use debug lib
-  // for (Index = 0; Index < PcrValues.count; Index++) {
-  //   DEBUG ((
-  //     DEBUG_INFO,
-  //     "ReadPcr - HashAlg = 0x%04x, Pcr[%02d], digest = ",
-  //     PcrSelectionOut.pcrSelections[Index].hash,
-  //     PcrIndex
-  //     ));
+  for (Index = 0; Index < PcrValues.count; Index++) {
+    DEBUG ((
+      DEBUG_INFO,
+      "ReadPcr - HashAlg = 0x%04x, Pcr[%02d], digest = ",
+      PcrSelectionOut.pcrSelections[Index].hash,
+      PcrIndex
+      ));
 
-  //   for (Index2 = 0; Index2 < PcrValues.digests[Index].size; Index2++) {
-  //     DEBUG ((DEBUG_INFO, "%02x ", PcrValues.digests[Index].buffer[Index2]));
-  //   }
+    for (Index2 = 0; Index2 < PcrValues.digests[Index].size; Index2++) {
+      DEBUG ((DEBUG_INFO, "%02x ", PcrValues.digests[Index].buffer[Index2]));
+    }
 
-  //   DEBUG ((DEBUG_INFO, "\n"));
-  // }
-  DumpPcrDigest (
-    PcrIndex,
-    PcrSelectionOut.pcrSelections[Index].hash,
-    &PcrValues
-    );
-  // MU_CHANGE END: Use debug lib
+    DEBUG ((DEBUG_INFO, "\n"));
+  }
 
   if (HashList != NULL) {
     CopyMem (
