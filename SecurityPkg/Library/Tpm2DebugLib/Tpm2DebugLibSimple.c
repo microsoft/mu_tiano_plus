13b98554d215cbd45bdfe2a18d96f136c963c1ea
Oliver Smith-Denny <osde@microsoft.com>
2025-07-14 10:52:35 -0700

    Revert "SecurityPkg: Tpm2DebugLib: Clean Up Foundations"
    This reverts commit e389830f866bdf19efa57b86a6dfd10c57e75cd7,
    as it has been upstreamed in a different form.

--- SecurityPkg/Library/Tpm2DebugLib/Tpm2DebugLibSimple.c
+++ SecurityPkg/Library/Tpm2DebugLib/Tpm2DebugLibSimple.c
@@ -7,9 +7,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
-#include <Uefi.h>
 #include <Library/DebugLib.h>
-#include <Library/Tpm2DebugLib.h>
 
 /**
   This function dumps as much information as possible about
