cd7fb9d6a3d9e72bf60590a85b0c258f111555b7
rdiaz <raymonddiaz@microsoft.com>
2025-08-27 00:59:28 +0000

    Decoupling Tpm2Help.c from Tpm2CommandLib by moving it to its own library: Tpm2HelpLib. This allows for the helper functions to be used without the need to include Tpm2CommandLib and Tpm2DeviceLib.

--- SecurityPkg/Library/Tpm2HelpLib/Tpm2Help.c
+++ SecurityPkg/Library/Tpm2HelpLib/Tpm2Help.c
@@ -1,14 +1,14 @@
 /** @file
   Implement TPM2 help.
 
+Copyright (c), Microsoft Corporation.
 Copyright (c) 2013 - 2018, Intel Corporation. All rights reserved. <BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
 #include <IndustryStandard/UefiTcgPlatform.h>
-#include <Library/Tpm2CommandLib.h>
-#include <Library/Tpm2DeviceLib.h>
+#include <Library/Tpm2HelpLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
