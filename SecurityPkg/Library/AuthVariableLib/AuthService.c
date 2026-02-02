49ce7aaf27b088cc17ef0d3c879b5e0f78adc1a3
Kun Qin <kun.qin@microsoft.com>
2026-02-02 23:25:14 +0000

    handle 0 entry dbx

--- SecurityPkg/Library/AuthVariableLib/AuthService.c
+++ SecurityPkg/Library/AuthVariableLib/AuthService.c
@@ -596,7 +596,7 @@ CheckSignatureListFormat (
 
       CertData = (EFI_SIGNATURE_DATA *)((UINT8 *)SigList + sizeof (EFI_SIGNATURE_LIST) + SigList->SignatureHeaderSize);
       CertLen  = SigList->SignatureSize - sizeof (EFI_GUID);
-      if (!RsaGetPublicKeyFromX509 (CertData->SignatureData, CertLen, &RsaContext)) {
+      if ((CertLen > 0) && !RsaGetPublicKeyFromX509 (CertData->SignatureData, CertLen, &RsaContext)) {
         RsaFree (RsaContext);
         return EFI_INVALID_PARAMETER;
       }
